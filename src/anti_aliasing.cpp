5ad5651f47a9e55625943f9291982cdd6b7e8dd4
fniyaz <35842362+fniyaz@users.noreply.github.com>
2020-03-20 22:36:59 +0300

    Lab 7

--- src/anti_aliasing.cpp
+++ src/anti_aliasing.cpp
@@ -10,4 +10,18 @@ AntiAliasing::~AntiAliasing()
 
 void AntiAliasing::DrawScene()
 {
+    camera.SetRenderTargetSize(width*2, height*2);
+
+    #pragma omp parallel for
+    for (short x = 0; x < width; x++) {
+        #pragma omp parallel for
+        for (short y = 0; y < height; y++) {
+            auto payload0 = TraceRay(camera.GetCameraRay(2*x + 1, 2*y), raytracing_depth).color;
+            auto payload1 = TraceRay(camera.GetCameraRay(2*x, 2*y + 1), raytracing_depth).color;
+            auto payload2 = TraceRay(camera.GetCameraRay(2*x + 1, 2*y + 1), raytracing_depth).color;
+            auto payload3 = TraceRay(camera.GetCameraRay(2*x, 2*y), raytracing_depth).color;
+
+            SetPixel(x, y, (payload0 + payload1 + payload2 + payload3) / 4);
+        }
+    }
 }
